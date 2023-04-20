1f6e25bede9fd29c5650374fe0aacd06f0f97732
Anastasia Holubnycha <anastastasia.holubnycha.cpamatica.io>
2023-04-20 10:58:03 +0300

    add prod without adding picture(in process)

--- controller/add-products-controller.php
+++ controller/add-products-controller.php
@@ -1,9 +1,17 @@
 <?php
+function clearData($val = ""): string
+{
+    $val = trim($val);
+    $val = strip_tags($val);
+    $val = stripslashes($val);
+    return htmlspecialchars($val);
+}
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $productTitle = $_POST['productTitle'];
-    $productDescription = $_POST['productDescription'];
-    $productPrice = $_POST['productPrice'];
-    $catId = $_POST['catId'];
+
+    $productTitle = clearData($_POST['productTitle']);
+    $productDescription = clearData($_POST['productDescription']);
+    $productPrice = clearData($_POST['productPrice']);
+    $catId = clearData($_POST['catId']);
 
     require_once __DIR__ . '/../model/add-product-model.php';
     $success = addProducts($productTitle, $productDescription, $productPrice, $catId);
